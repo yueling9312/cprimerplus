4cb696b949c9aa0503d53d90e84ac128a4d001d0
yue ling <yl@debian>
2018-06-05 23:57:01 +0800

    5.8

---
+++ example/5/5.8.c
@@ -0,0 +1,12 @@
+//sizeof size_z
+#include <stdio.h>
+int main(void)
+{
+	int n = 0;
+	size_t intsize;
+
+	intsize=sizeof(int) ;
+	printf("n=%d, n has %zd bytes; all ints have %zd bytes.\n",n, sizeof n, intsize);
+	
+	return 0;
+}
